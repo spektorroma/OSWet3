e9f4b1b9e90141b339d4bad1a8e68f8b77ec08c0
Semyon <gsemyong@gmail.com>
2022-01-15 11:44:56 +0000

    overload testing

---
+++ stress.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html &
+./client 5003 home.html 
+
